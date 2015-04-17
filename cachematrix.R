f408dcad266a31154beaf329427eeda3ec9a3c54
anon2150 <anon2150@users.noreply.github.com>
2015-04-17 16:32:37 -0600

    Working version for matrix inverse Uses 'inverse' for set_m_inv function Very similar to the sample given in the course, just changing variable and functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+###########################################################################
+#       ASSIGNMENT 2 - LEXICAL SCOPING
+###########################################################################
 
-## Write a short comment describing this function
 
+##  This function creates a special "matrix" object 
+##  that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse        
+        
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        set_m_inv <- function(inverse) m <<- inverse
+        get_m_inv <- function() m
+        list(set = set, get=get,
+             set_m_inv = set_m_inv,
+             get_m_inv = get_m_inv)
 }
 
-
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned by
+##  makeCacheMatrix above. If the inverse has already been calculated (and the
+##  matrix has not changed), then the cachesolve should retrieve the inverse
+##  from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$get_m_inv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$set_m_inv(m)
+        m
 }
